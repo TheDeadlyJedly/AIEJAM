9d2079b51747f5b6bab6c8997908932a75c1b73a
Alex Pipe <AlexDP2000@outlook.com>
2018-09-30 06:22:51 +0930

    Added pause menu and other stuff

---
+++ Assets/MenuController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour {
+
+    public GameObject pause;
+
+    public void QuitGame() {
+        Application.Quit();
+    }
+    public void StartGame() {
+        SceneManager.LoadScene("Greybox");
+    }
+    public void MainMenu() {
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape) && pause.activeInHierarchy == false) {
+            pause.SetActive(true);
+            Time.timeScale = 0;
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape) && pause.activeInHierarchy == true) {
+            Unpause();
+        }
+    }
+    public void Unpause() {
+        Time.timeScale = 1;
+        pause.SetActive(false);
+    }
+}
