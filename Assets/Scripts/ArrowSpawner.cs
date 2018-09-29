5233fe529aef0221073c3ece45dad5f0cf53637b
AIE\s163203 <JedCamel@gmail.com>
2018-09-30 01:17:36 +0930

    FINAL BOW AND ARROW ANIMATION & MECHANICS IMPLEMENTED - Level Building Time

---
+++ Assets/Scripts/ArrowSpawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowSpawner : MonoBehaviour {
+
+	public GameObject arrow;
+	public GameObject bow;
+	public Transform bowPos;
+	public int projectileSpeed = 10;
+
+	private Bow bowReference;
+
+	void SpawnArrow()
+	{
+		Rigidbody fireArrow;
+		fireArrow = Instantiate (arrow, bowPos.position, bowPos.rotation).GetComponent<Rigidbody> ();
+		fireArrow.AddForce (transform.forward * projectileSpeed, ForceMode.Impulse);
+
+	}
+}
--- Assets/Scripts/Bow.cs
+++ Assets/Scripts/Bow.cs
@@ -2,29 +2,51 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Events;
 
 public class Bow : MonoBehaviour {
 
-    public GameObject arrow;
-    public GameObject bow;
-    public Transform bowPos;
-    public int projectileSpeed = 10;
-
     public float coolDown = 1;
     public Slider coolDownSlider;
+
+	private bool stringPulled = false;
+
+	public UnityEvent onPull;
+	public UnityEvent onRelease;
+
+	void Start()
+	{
+
+	}
 	
 	// Update is called once per frame
 	void Update () {
         if (coolDown < 1) coolDown += Time.deltaTime;
         //Debug.Log("Cooldown: " + coolDown);
         if(coolDownSlider != null) coolDownSlider.value = coolDown;
-        if (Input.GetKeyDown(KeyCode.Mouse0) && (coolDown >= 1)) {
-            coolDown = 0;
-            Rigidbody fireArrow;
-            fireArrow = Instantiate(arrow, bowPos.position, bowPos.rotation).GetComponent<Rigidbody>();
-            fireArrow.AddForce(transform.forward * projectileSpeed, ForceMode.Impulse);
 
-            
-        }
+//		if (Input.GetKeyDown(KeyCode.Mouse0) && (coolDown >= 1)) {
+//			coolDown = 0;
+//			Rigidbody fireArrow;
+//			fireArrow = Instantiate(arrow, bowPos.position, bowPos.rotation).GetComponent<Rigidbody>();
+//			fireArrow.AddForce(transform.forward * projectileSpeed, ForceMode.Impulse);
+//			stringPulled = true;
+//			onPull.Invoke ();
+//		}
+
+		if (coolDown >= 1)
+		{
+			if (stringPulled == false && Input.GetKey (KeyCode.Mouse0)) 
+			{
+				onPull.Invoke ();
+				stringPulled = true;
+			} 
+			else if (stringPulled == true && !Input.GetKey (KeyCode.Mouse0)) 
+			{
+				coolDown = 0;
+				stringPulled = false;
+				onRelease.Invoke ();
+			}
+		}
 	}
 }
