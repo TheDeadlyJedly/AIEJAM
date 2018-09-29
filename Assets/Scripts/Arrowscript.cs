f63b20b3199bf3d0b1d478dbc700be8494070b63
Scott <sloth.13@hotmail.com>
2018-09-29 18:59:08 +0930

    Arrow update

--- Assets/Scripts/Arrowscript.cs
+++ Assets/Scripts/Arrowscript.cs
@@ -4,19 +4,42 @@ using UnityEngine;
 
 public class Arrowscript : MonoBehaviour {
 
-    private void Awake() {
-        Invoke("DestroyArrow", 5f);
-    }
 
-    public void DestroyArrow() {
-        Destroy(gameObject);
+    TrailRenderer _renderer;
+    float _renTimer;
+    private void Awake() {
+        Destroy(gameObject, 5);
+        
+        
+        _renderer = GetComponentInChildren<TrailRenderer>();
+        _renTimer = _renderer.time;
+        Destroy(_renderer.gameObject, 5);
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("HIT");
+        _renderer.transform.SetParent(null);
         IHitable hit = collision.gameObject.GetComponent<IHitable>();
         if (hit != null)
+        {
             hit.Hit(1);
+        }
+
+        GetComponent<Rigidbody>().isKinematic = true;
+        transform.SetParent(collision.transform);
+        GetComponent<Collider>().enabled = false;
+
+        StartCoroutine(Fade(5));
+    }
+
+    public IEnumerator Fade(int time)
+    {
+        float timer = time;
+        while(time > 0)
+        {
+            timer -= Time.deltaTime;
+            _renderer.time = Mathf.Lerp(0, _renTimer, timer);
+            yield return new WaitForEndOfFrame();
+        }
     }
 }
