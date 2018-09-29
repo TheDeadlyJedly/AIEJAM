df35836467c06cf197ec0fe19b533e6ff4b791ee
Alex Pipe <AlexDP2000@outlook.com>
2018-09-29 18:12:57 +0930

    Changes to level 2

---
+++ Assets/Fan.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fan : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+        gameObject.transform.Rotate(Vector3.up * 10);
+	}
+}
