5051277a5865cb18755052cae06d8eb54df0708a
Alex Pipe <AlexDP2000@outlook.com>
2018-09-29 12:25:32 +0930

    Changed bow mechanics

--- Assets/Scripts/Bow.cs
+++ Assets/Scripts/Bow.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Bow : MonoBehaviour {
 
@@ -8,14 +9,22 @@ public class Bow : MonoBehaviour {
     public GameObject bow;
     public Transform bowPos;
     public int projectileSpeed = 10;
+
+    public float coolDown = 1;
+    public Slider coolDownSlider;
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown(KeyCode.Mouse0)) {
+        coolDown -= Time.deltaTime;
+        Debug.Log("Cooldown: " + coolDown);
+        coolDownSlider.value = coolDown;
+        if (Input.GetKeyDown(KeyCode.Mouse0) && (coolDown <= 0)) {
+            coolDown = 1;
             Rigidbody fireArrow;
             fireArrow = Instantiate(arrow, bowPos.position, bowPos.rotation).GetComponent<Rigidbody>();
             fireArrow.AddForce(transform.forward * projectileSpeed, ForceMode.Impulse);
             //GetComponent<Rigidbody>().AddForce(Vector3.forward * projectileSpeed);
+            
         }
 	}
 }
