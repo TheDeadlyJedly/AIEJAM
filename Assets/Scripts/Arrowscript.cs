1db1e83569f91e6f6b42af2dcaf2115b07c32636
Alex Pipe <AlexDP2000@outlook.com>
2018-09-29 12:02:40 +0930

    Added second scene and bow + arrow

---
+++ Assets/Scripts/Arrowscript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrowscript : MonoBehaviour {
+
+    public GameObject arrow;
+    // Use this for initialization
+    //private void Awake() {
+    //    Invoke("DestroyArrow", 5f);
+    //}
+
+    //public void DestroyArrow() {
+    //    Destroy(arrow);
+    //}
+}
---
+++ Assets/Scripts/Bow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bow : MonoBehaviour {
+
+    public GameObject arrow;
+    public GameObject bow;
+    public Transform bowPos;
+    public int projectileSpeed = 10;
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Mouse0)) {
+            Rigidbody fireArrow;
+            fireArrow = Instantiate(arrow, bowPos.position, bowPos.rotation).GetComponent<Rigidbody>();
+            fireArrow.AddForce(transform.forward * projectileSpeed, ForceMode.Impulse);
+            //GetComponent<Rigidbody>().AddForce(Vector3.forward * projectileSpeed);
+        }
+	}
+}
