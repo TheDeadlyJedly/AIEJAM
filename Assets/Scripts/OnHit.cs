442b7935d64789722f968e373f18000f87dcc734
Scott <hartsa1993@gmail.com>
2018-09-30 14:09:38 +0930

    On Hit script

---
+++ Assets/Scripts/OnHit.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+public class OnHit : MonoBehaviour, IHitable
+{
+    public UnityEvent _onHit;
+    public bool Hit(int amt)
+    {
+        _onHit.Invoke();
+        return true;
+    }
+}
